e5b3acf85487e3f37f2656c33930b884ad89a278
haaanck <marouatayman@gmail.com>
2026-09-11 09:46:13 +0100

    Add files via upload

---
+++ tutorials/article.js
@@ -0,0 +1,6 @@
+let num1 = 10;
+let num2 = 5;
+console.log("10 * 5 =",num1*num2)
+console.log("10 + 5 =",num1+num2)
+console.log("10 - 5 =",num1-num2)
+console.log("10 / 5 =",num1/num2)
---
+++ tutorials/pre.js
@@ -0,0 +1,4 @@
+let fname = "Aymane"
+let lname = "Marouat"
+let formation = "Web Devlopment"
+console.log(fname,lname,formation)
