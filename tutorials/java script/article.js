3c583f703cd35dd80df121cacca102c921950c78
haaanck <marouatayman@gmail.com>
2026-09-21 11:52:18 +0100

    Delete tutorials/java script directory

--- tutorials/java script/article.js
+++
@@ -1,6 +0,0 @@
-let num1 = 10;
-let num2 = 5;
-console.log("10 * 5 =",num1*num2)
-console.log("10 + 5 =",num1+num2)
-console.log("10 - 5 =",num1-num2)
-console.log("10 / 5 =",num1/num2)
--- tutorials/java script/pre.js
+++
@@ -1,4 +0,0 @@
-let fname = "Aymane"
-let lname = "Marouat"
-let formation = "Web Devlopment"
-console.log(fname,lname,formation)
